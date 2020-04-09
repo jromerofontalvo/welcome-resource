8fbf242d767808f5a5d7a4dabaafc5c139ab7924
jromerofontalvo <jromerofontalvo@zapatacomputing.com>
2020-04-09 16:44:14 -0400

    playing with setup

--- src/setup.py
+++ src/setup.py
@@ -1,13 +1,19 @@
 import setuptools
 
+# setuptools.setup(
+#     name                            = "orquestra",
+#     packages                        = setuptools.find_packages(where = "python"),
+#     package_dir                     = {
+#         "" : "python"
+#     },
+#     classifiers                     = (
+#         "Programming Language :: Python :: 3",
+#         "Operating System :: OS Independent",
+#     ),
+# )
+
 setuptools.setup(
-    name                            = "orquestra",
-    packages                        = setuptools.find_packages(where = "python"),
-    package_dir                     = {
-        "" : "python"
-    },
-    classifiers                     = (
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ),
+    name="orquestra",
+    version="0.1",
+    packages=setuptools.find_packages(),
 )
