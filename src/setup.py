42b8dc22d5b31da458446798ad76c48531f11ecf
jromerofontalvo <jromerofontalvo@zapatacomputing.com>
2020-04-09 17:33:10 -0400

    another fix

--- src/setup.py
+++ src/setup.py
@@ -1,10 +1,24 @@
 import setuptools
 
+import setuptools
+
 setuptools.setup(
-    name="welcome-resource",
-    version="0.1.0",
-    py_modules=['myorquestra']
+    name                            = "myorquestra",
+    packages                        = setuptools.find_packages(where = "python"),
+    package_dir                     = {
+        "" : "python"
+    },
+    classifiers                     = (
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ),
 )
+
+# setuptools.setup(
+#     name="welcome-resource",
+#     version="0.1.0",
+#     py_modules=['myorquestra']
+# )
 
 # setuptools.setup(
 #     name="orquestra",
