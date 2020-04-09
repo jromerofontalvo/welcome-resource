680de6b95f4f08a4549af71738d883523211aa42
jromerofontalvo <jromerofontalvo@zapatacomputing.com>
2020-04-09 16:27:37 -0400

    added chem example resource

--- src/python/orquestra/welcome.py
+++ src/python/orquestra/welcome.py
@@ -13,3 +13,22 @@ def welcome():
 
     with open("welcome.json",'w') as f:
         f.write(json.dumps(message_dict, indent=2)) # Write message to file as this will serve as output artifact
+
+def create_diatomic_molecule_geometry(species1, species2, bond_length):
+    """Create a molecular geometry for a diatomic molecule.
+    
+    Args:
+        species1 (str): Chemical symbol of the first atom, e.g. 'H'.
+        species2 (str): Chemical symbol of the second atom.
+        bond_length (float): bond distance.
+        
+    Returns:
+        dict: a dictionary containing the coordinates of the atoms.
+    """
+
+    geometry = {"sites": [
+        {'species': species1, 'x': 0, 'y': 0, 'z': 0},
+        {'species': species2, 'x': 0, 'y': 0, 'z': bond_length}
+    ]}
+
+    return geometry
